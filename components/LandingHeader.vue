9520f6f6123382091e789578967ba0941e403186
logic envy <nutthawut.y@synergysoftware.co.th>
2020-11-04 21:45:01 +0700

    init project

---
+++ components/LandingHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <!-- As a heading -->
+    <b-navbar toggleable :type="isScroll ? 'dark' : 'light'" :variant="isScroll ? 'dark' : 'light'" fixed="top">
+      <b-navbar-brand tag="h1" class="mb-0">{{ Title }}</b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-button size="md" class="my-2 my-sm-0" :variant="isScroll ? 'outline-success' : 'success'" :href="Link" target="_blank">สมัครสมาชิก</b-button>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+<script>
+export default {
+    props: {
+      title: String,
+      link: String,
+    },
+    computed: {
+      Title () {
+        return !this.title ? 'Landing menu' : this.title;
+      },
+      Link () {
+        return !this.link ? 'https://www.google.co.th/' : this.link;
+      }
+    },
+    data () {
+      return {
+        isScroll: false
+      }
+    },
+    created () {
+      window.addEventListener('scroll', this.handleScroll);
+    },
+    destroyed () {
+      window.addEventListener('scroll', this.handleScroll);
+    },
+    methods: {
+      handleScroll (event) {
+        if (document.documentElement.scrollTop <= 1) {
+          this.isScroll = false;
+        } else {
+          this.isScroll = true;
+        }
+        // console.log('--> scroll:', event, document.documentElement.scrollTop);
+      }
+    }
+}
+</script>
---
+++ components/LineButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="links">
+        <div class="text-center mt-4">
+            <a href="https://lin.ee/yQcI9pO" rel="nofollow"><img src="~/assets/images/add-line.png" width="250px" alt="Add line"></a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.links {
+  padding-top: 15px;
+}
+</style>
