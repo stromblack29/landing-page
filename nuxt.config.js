0ebe04ebf40cfacc18dad1f532331822ebb4e7a2
logic envy <nutthawut.y@synergysoftware.co.th>
2020-11-05 11:33:54 +0700

    compact file babel

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,12 +30,12 @@ export default {
   buildModules: [
     'bootstrap-vue/nuxt'
   ],
-
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-  }
+    babel: { compact: true }
+  },
 }
